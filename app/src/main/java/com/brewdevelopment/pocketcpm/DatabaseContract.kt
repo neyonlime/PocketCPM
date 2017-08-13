e5072287bf631b730c1cbfa62fff2d36fb55afbd
Ashkan Abedian <neyonlime@gmail.com>
2017-08-13 15:19:20 -0400

    added contract class for data base

---
+++ app/src/main/java/com/brewdevelopment/pocketcpm/DatabaseContract.kt
@@ -0,0 +1,14 @@
+package com.brewdevelopment.pocketcpm
+
+import android.provider.BaseColumns
+
+/**
+ * Created by neyon on 2017-08-13.
+ */
+
+class DatabaseContract{
+    //inner class for table one
+    companion object FeedEntry: BaseColumns {
+
+    }
+}
