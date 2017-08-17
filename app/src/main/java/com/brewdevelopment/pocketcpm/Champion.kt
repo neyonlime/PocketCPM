50a74907fb53d1a0c4c34d46b54abf0d12559846
Ashkan Abedian <neyonlime@gmail.com>
2017-08-16 22:12:27 -0400

    test commit from mac

--- app/src/main/java/com/brewdevelopment/pocketcpm/Champion.kt
+++ app/src/main/java/com/brewdevelopment/pocketcpm/Champion.kt
@@ -1,7 +1,7 @@
 package com.brewdevelopment.pocketcpm
 
 /**
- * Created by neyonlime on 2017-08-15.
+ * Created by neyonlimel on 2017-08-15.
  */
 class Champion(var name: String){
     lateinit var ID: String
